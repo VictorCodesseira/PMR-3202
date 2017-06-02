fce0ae70c8008a283954e1e5dc866a538a980ce5
jvSanches <syxtroz@gmail.com>
2017-06-02 11:33:56 -0300

    Motors and servo now work on BT

--- Carrinho.ino
+++ Carrinho.ino
@@ -5,7 +5,7 @@
 void initBT();
 void setMotors(int motor_left, int motor_right);
 long readUS();
-int readBT_pack(int leitura[9]);
+int readBT_pack(int leitura[5]);
 int readBT();
 
 #define MOTOR_L 9
@@ -32,13 +32,12 @@ int readBT();
 #define THRESHOLD 500
 
 Servo myservo;
+SoftwareSerial bluetooth(RX, TX);
 
 int sens_esq = 0, sens_dir = 0, dist = 0;
 int x, y, z, luz, buzina;
 int autonomo = 1, controlado = 1;
-int pack[9] = {255, 9, 127, 127, 0, 0, 0, 254, 254};
 
-SoftwareSerial bluetooth(RX, TX);
 
 void initBT(){
     bluetooth.begin(9600);
@@ -75,19 +74,15 @@ long readUS(){
     return cm;
 }
 
-int readBT_pack(int leitura[9]){
+int readBT_pack(int leitura[5]){
     if (!(bluetooth.available())) return 0;
     int c = bluetooth.read();
     if (c != 255) return 0;
-    while (!(bluetooth.available()));
-    int length = bluetooth.read();
-    for (int i = 1; length; i++){
+    for (int i = 1; i<5 ; i++){
         while(!(bluetooth.available()));
         leitura[i] = bluetooth.read();
     }
-    if (leitura[8] != 254) return 0;
-    int soma = leitura[2] + leitura[3] + leitura[4] + leitura[5] + leitura[6];
-    if (leitura[7] != soma) return 0;
+    if (leitura[4] != 254) return 0;
     return 1;
 }
 
@@ -105,21 +100,26 @@ void setup() {
     pinMode(IR_R, INPUT);
     pinMode(TRIG, OUTPUT);
     pinMode(ECHO, INPUT);
+    pinMode(13, OUTPUT);
+    
+    digitalWrite(13, HIGH);
 
     SoftPWMBegin();
     SoftPWMSet(MOTOR_L, 0);
     SoftPWMSet(MOTOR_R, 0);
     Serial.begin(9600);
-
     myservo.attach(SERVO);
     initBT();
+
+    digitalWrite(13, LOW);
 }
 
 // the loop function runs over and over again forever
-int leitura, motor_l, motor_r, angulo = 90;
+int leitura[5] = {255, 0, 0, 84, 254} , motor_l, motor_r, angulo = 90;
 void loop() {
-    if (Serial.available()){
-        leitura = Serial.read();
+    /*
+    if (bluetooth.available()){ //batatapython
+        leitura = bluetooth.read();
         switch (leitura){
             case 'r':
                 motor_l = 0;
@@ -142,10 +142,10 @@ void loop() {
                 motor_r = -255;
                 break;
             case 'q':
-                angulo -= 5;
+                angulo = 60;
                 break;
             case 'e':
-                angulo += 5;
+                angulo = 120;
                 break;
         }
         angulo = constrain(angulo, 0, 180);
@@ -158,6 +158,7 @@ void loop() {
         Serial.print(" Angulo: ");
         Serial.println(angulo);
         }
+        */
     // while (autonomo == 1){
     //     sens_esq = analogRead(IR_L);
     //     sens_dir = analogRead(IR_R);
@@ -174,16 +175,18 @@ void loop() {
 
     // setMotors(0,0);
     // delay(1000);
-
-    // while (controlado == 1){
-    //     if (readBT_pack(pack)!=0){
-    //         x = 2*(pack[2]-127);
-    //         y = 2*(pack[3]-127);
-    //         z = pack[4];
-    //         luz = pack[5];
-    //         buzina = pack[6];
-    //         setMotors(constrain(y+x, -255, 255), constrain(y-x, -255, 255));
-    //         myservo.write(map(z, 0, 255, 0, 180));
-    //     }
-    // }
+    int last_z = 84;
+    while (controlado == 1){
+        if (readBT_pack(leitura)){         
+            x = 2*(leitura[1]-127);
+            y = 2*(leitura[2]-127);
+            z = leitura[3];
+            setMotors(constrain(y+x, -255, 255), constrain(y-x, -255, 255));
+            Serial.println(z);
+            if (z != last_z){
+                myservo.write(z);
+                last_z = z;
+            }            
+       }
+    }
 }
--- Controle.py
+++ Controle.py
@@ -39,6 +39,8 @@ def play():
     last_pack = [127, 127, 0, 0, 0]
 
     while not done:
+        while(device.in_waiting > 0):
+            print(device.readline())
         for event in pygame.event.get(): # User did something
             if event.type == pygame.QUIT: # If user clicked close
                 done = True # Flag that we are done so we exit this loop
@@ -46,12 +48,18 @@ def play():
         luz = buzina = 0
 
         #for event in pygame.event.get(): # User did something
-        x = int(127 + (127 * (joystick.get_axis(1) - xtrim)))
-        y = int(127 + (127 * (joystick.get_axis(0) - ytrim)))
+        x = int(127 + (127 * (joystick.get_axis(0) - ytrim)))
+        y = int(127 + (127 * -(joystick.get_axis(1) - xtrim)))
         z = int(127 + (127 * (joystick.get_axis(2) - ztrim)))
+        if x > 253:
+            x = 253
+        if y > 253:
+            y = 253
+        if z > 253:
+            z = 253
 
         if joystick.get_button(0):
-            z = 255
+            z = 0
         if joystick.get_button(2):
             luz = 1
         if joystick.get_button(3):
@@ -67,11 +75,14 @@ def play():
 
         chksum = (x + y + z + luz + buzina)%256
 
-        pack = [255, 9, x, y, z, luz, buzina, chksum, 254]
-
+        z = ((z*50)/255)+60
+        z = int(z)
+        pack = [255, x, y, z, 254]
         if pack != last_pack:
+            print(pack)
             send(pack)
             last_pack = pack
         clock.tick(20)
-
+connect()
 play()
+kill()
